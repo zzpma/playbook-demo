e96391697e0d0a6e329af262fed558ac5ddc9c11
zzpma <zz.pma2000@gmail.com>
2025-12-08 21:00:09 +0800

    feat(db): add PostgreSQL config for production environment

--- playbook_demo/settings.py
+++ playbook_demo/settings.py
@@ -135,12 +135,25 @@ SOCIALACCOUNT_PROVIDERS = {
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': env('PGDATABASE', default=None),
+            'USER': env('PGUSER', default=None),
+            'PASSWORD': env('PGPASSWORD', default=None),
+            'HOST': env('PGHOST', default=None),
+            'PORT': env('PGPORT', default=None),
+        }
+    }
+
 
 
 # Password validation
